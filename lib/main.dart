7dc4bef863c3816c26639ddf89886499f0a0405f
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-06 21:44:54 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,13 +32,106 @@ class HomeScren extends StatelessWidget {
       ),
       body:ListView(
        children: [
+         ContactCard(),
          Card(
            child: Row(
              children: [
                Padding(
                  padding: const EdgeInsets.all(8.0),
                  child: CircleAvatar(
-                   radius: 30,
+                   radius: 25,
+                   backgroundColor: Colors.green,
+                 ),
+               ),
+               SizedBox(width: 10,),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("RaFiul RaZu",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w700),),
+                   SizedBox(height: 5),
+                   Text("0182663212",style: TextStyle(fontWeight: FontWeight.w600),),
+                 ],
+               ),
+             ],
+           ),
+
+         ),
+         Card(
+           child: Row(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: CircleAvatar(
+                   radius: 25,
+                   backgroundColor: Colors.green,
+                 ),
+               ),
+               SizedBox(width: 10,),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("RaFiul RaZu",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w700),),
+                   SizedBox(height: 5),
+                   Text("0182663212",style: TextStyle(fontWeight: FontWeight.w600),),
+                 ],
+               ),
+             ],
+           ),
+
+         ),
+         Card(
+           child: Row(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: CircleAvatar(
+                   radius: 25,
+                   backgroundColor: Colors.green,
+                 ),
+               ),
+               SizedBox(width: 10,),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("RaFiul RaZu",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w700),),
+                   SizedBox(height: 5),
+                   Text("0182663212",style: TextStyle(fontWeight: FontWeight.w600),),
+                 ],
+               ),
+             ],
+           ),
+
+         ),
+         Card(
+           child: Row(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: CircleAvatar(
+                   radius: 25,
+                   backgroundColor: Colors.green,
+                 ),
+               ),
+               SizedBox(width: 10,),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("RaFiul RaZu",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w700),),
+                   SizedBox(height: 5),
+                   Text("0182663212",style: TextStyle(fontWeight: FontWeight.w600),),
+                 ],
+               ),
+             ],
+           ),
+
+         ),
+         Card(
+           child: Row(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: CircleAvatar(
+                   radius: 25,
                    backgroundColor: Colors.green,
                  ),
                ),
@@ -57,6 +150,39 @@ class HomeScren extends StatelessWidget {
          ),
        ],
       ),
+    );
+  }
+}
+
+class ContactCard extends StatelessWidget {
+  const ContactCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.green,
+            ),
+          ),
+          SizedBox(width: 10,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("RaFiul RaZu",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w700),),
+              SizedBox(height: 5),
+              Text("0182663212",style: TextStyle(fontWeight: FontWeight.w600),),
+            ],
+          ),
+        ],
+      ),
+
     );
   }
 }
