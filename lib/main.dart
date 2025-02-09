cf9830216c455cf2d59f23de86afa560b92d1ac8
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-09 20:27:07 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,19 +31,33 @@ class HomeScren extends StatelessWidget{
       appBar: AppBar(
         title: Text("Gird View Design"),
       ),
-      body: GridView.builder(
-        itemCount: 10,
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent:120),
-          itemBuilder: (context,index){
-            return Card(
-              color: Colors.amber,
-              child: SizedBox(
-                height: 50,
-                width: 50,
-                child: Center(child: Text("Razu")),
-              ),
-            );
-          },
+      body:Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width:double.infinity,
+              color: Colors.green,
+            ),
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics,
+            itemCount: 10,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent:120),
+            itemBuilder: (context,index){
+              return Card(
+                color: Colors.amber,
+                child: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: Center(child: Text("Razu")),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
