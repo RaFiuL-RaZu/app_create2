e2b2ce1402680ea896dc930316194c1890b7b42f
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-09 19:13:48 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,154 +33,54 @@ class HomeScren extends StatelessWidget{
       ),
       body: ListView(
         children: [
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor:Colors.white,
-                    backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("RaFiuL RaZu",style: TextStyle(fontWeight: FontWeight.w800),),
-                    SizedBox(height: 5,),
-                    Text("01826851248"),
-                  ],
-                )
-              ],
-            ),
-          ),
+          ContactList(name:"RaFi" ,phone:"01632212211" ,),
+          ContactList(name:"RaFiuL",phone: "01632212211",),
+          ContactList(name:"Sazu" ,phone: "01632212211",),
+          ContactList(name:"Razu" ,phone:"01632212211" ,),
+          ContactList(name: "SaFin",phone: "01632212211",),
+          ContactList(name: "Sazid",phone: "01632212211",),
+
+
         ],
       )
     );
   }
 
 
+}
+
+class ContactList extends StatelessWidget {
+  ContactList({
+    Key? key,required this.name, required this.phone,
+  }) : super(key: key);
+  final String name;
+  final String phone;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor:Colors.white,
+              backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBQcRtWVDjSpn8k9DDCETUsh-LL3HVJGDMaQ&s"),
+            ),
+          ),
+          SizedBox(width: 10,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name,style: TextStyle(fontWeight: FontWeight.w800),),
+              SizedBox(height: 5,),
+              Text(phone),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
