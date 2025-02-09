2ece9224ed4bbd3f58aa9c374a5d2b6943fbbf4e
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-09 20:06:11 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,57 +31,19 @@ class HomeScren extends StatelessWidget{
       appBar: AppBar(
         title: Text("Gird View Design"),
       ),
-      body: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 2,
-
-          ),
-        children: [
-          Card(
-            color: Colors.green,
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(child: Text("Razu")),
-            ),
-          ),
-          Card(
-            color: Colors.amber,
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(child: Text("Razu")),
-            ),
-          ),
-          Card(
-            color: Colors.amber,
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(child: Text("Razu")),
-            ),
-          ),
-          Card(
-            color: Colors.amber,
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(child: Text("Razu")),
-            ),
-          ),
-          Card(
-            color: Colors.amber,
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(child: Text("Razu")),
-            ),
-          ),
-
-        ],
+      body: GridView.builder(
+        itemCount: 10,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (context,index){
+            return Card(
+              color: Colors.amber,
+              child: SizedBox(
+                height: 50,
+                width: 50,
+                child: Center(child: Text("Razu")),
+              ),
+            );
+          },
       ),
     );
   }
