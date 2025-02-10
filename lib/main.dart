4482a25876983d8bf1360cb91f5ec3b475fb5f7e
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-10 15:26:43 +0600

    grid commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,72 +23,30 @@ class MyApp extends StatelessWidget{
 class HomeScren extends StatelessWidget{
   HomeScren({Key? key}) : super(key: key);
 
-  List<Map> studentInfo=[
-    {"name":"Razu","dept":"CSE","Id":"102621"},
-    {"name":"Sazu","dept":"CE","Id":"102621"},
-    {"name":"RaFi","dept":"MME","Id":"102621"},
-    {"name":"RaKi","dept":"EEE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-    {"name":"Raz","dept":"TE","Id":"102621"},
-
-  ];
 
   @override
   Widget build(BuildContext context) {
 
 
     return Scaffold(
-      body:SingleChildScrollView(
-        child: Column(
+      body:Stack(
+        clipBehavior: Clip.none,
           children: [
             Container(
-              height: 150,
-              width: double.infinity,
+              height: 200,
+              width:double.infinity,
               color: Colors.red,
+              child: Text("DaTa"),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GridView.builder(
-                shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: studentInfo.length,
-                  gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 150,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                  ),
-                  itemBuilder:(context,index){
-                    return Card(
-                      color: Colors.green,
-                      child: SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: Center(
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Center(child: Text(studentInfo[index]["name"])),
-                                Center(child: Text(studentInfo[index]["dept"])),
-                                Center(child: Text(studentInfo[index]["Id"])),
-
-                              ],
-                            ),
-                          ),
-                        ),
-
-                      ),
-                    );
-                  }
-              ),
-            ),
+            Positioned(
+              bottom: -40,
+                left: 30,
+                child:CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.green,
+                )),
           ],
-
         ),
-      ),
     );
   }
 
